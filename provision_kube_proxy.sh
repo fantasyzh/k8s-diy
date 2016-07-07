a89f56a10f6a4380e78868d02f1e42d58b784091
yinghui <lyh@lyh-Mac-Air.local>
2016-07-08 00:05:48 +0800

    kube-proxy as service

--- provision_kube_proxy.sh
+++ provision_kube_proxy.sh
@@ -1,14 +1,24 @@
+set -e
+set -x
+
 role=$1
-master_ip=$1
+master_ip=$2
 
 if [ $role == "master" ]; then
     master_endpoint="http://127.0.0.1:8080"
+    specific_flags=
 else
     master_endpoint="https://$master_ip"
+    specific_flags="--kubeconfig=/etc/kubernetes/worker-kubeconfig.yaml"
     cp /vagrant/worker-kubeconfig.yaml /etc/kubernetes/worker-kubeconfig.yaml
 fi
-image_tag=`cat /vagrant/kubernetes/server/bin/kube-proxy.docker_tag`
-docker load -i /vagrant/kubernetes/server/bin/kube-proxy.tar
-sed -e "s#{{ master_endpoint }}#$master_endpoint#g" -e "s/{{ proxy_image_tag }}/$image_tag/g" /vagrant/kube-proxy.yaml > /tmp/kube-proxy.yaml
-cp /tmp/kube-proxy.yaml /etc/kubernetes/manifests/kube-proxy.yaml
 
+systemctl stop kube-proxy.service || true
+
+install -d /opt/kube/
+cp /vagrant/kubernetes/server/bin/kube-proxy /opt/kube/kube-proxy
+
+sed -e "s#{{ master_endpoint }}#$master_endpoint#g" -e "s#{{ specific_flags }}#$specific_flags#g" /vagrant/kube-proxy.service > /tmp/kube-proxy.service
+cp /tmp/kube-proxy.service /etc/systemd/system/kube-proxy.service
+systemctl enable kube-proxy.service
+systemctl restart kube-proxy.service
