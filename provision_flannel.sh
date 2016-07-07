e4bac6fb10c09c2e2ebcdaca4c83e4849bbe8a54
yinghui <lyh@lyh-Mac-Air.local>
2016-07-07 14:28:21 +0800

    flannel.service -> flanneld.service

--- provision_flannel.sh
+++ provision_flannel.sh
@@ -11,7 +11,10 @@ sed "s#{{ pod_network }}#$pod_network#g" /vagrant/flannel_config.json > /tmp/fla
 flannel_release=flannel-0.5.5
 install -d /opt/flannel
 install /vagrant/$flannel_release/flanneld /opt/flannel/flanneld
-sed "s/{{ master_ip }}/$master_ip/g" /vagrant/flannel.service > /tmp/flannel.service
-cp /tmp/flannel.service /etc/systemd/system/flannel.service
-systemctl enable flannel
-systemctl start flannel
+sed "s/{{ master_ip }}/$master_ip/g" /vagrant/flanneld.service > /tmp/flanneld.service
+systemctl stop flanneld
+systemctl disable flanneld
+cp /tmp/flanneld.service /etc/systemd/system/flanneld.service
+systemctl daemon-reload
+systemctl enable flanneld
+systemctl start flanneld
