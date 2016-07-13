281fd6574a9e7f947896b288050dca8b2baa7129
yinghui <yhliang86@hotmail.com>
2016-07-13 22:45:34 +0800

    wait etcd ready, and set flannel config

--- provision_etcd.sh
+++ provision_etcd.sh
@@ -6,7 +6,14 @@ etcd_release=etcd-v2.3.7-linux-amd64
 install -d /opt/etcd
 install /vagrant/$etcd_release/etcd /opt/etcd/etcd
 install /vagrant/$etcd_release/etcdctl /opt/etcd/etcdctl
-sed "s/{{ master_ip }}/$master_ip/g" -e "s/{{ pod_network }}/$pod_network/g" /vagrant/etcd2.service > /tmp/etcd2.service
+sed -e "s/{{ master_ip }}/$master_ip/g" /vagrant/etcd2.service > /tmp/etcd2.service
 cp /tmp/etcd2.service /etc/systemd/system/etcd2.service
 systemctl enable etcd2
 systemctl restart etcd2
+
+until /opt/etcd/etcdctl --endpoints http://$master_ip:2379 ls ; do
+    echo 'wait etcd ready...'
+    sleep 0.1
+done
+
+/opt/etcd/etcdctl --endpoints http://$master_ip:2379 set /coreos.com/network/config "{\"Network\": \"$pod_network\", \"Backend\": { \"Type\": \"udp\" } }"
